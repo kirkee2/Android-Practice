8c204e68da4e753ee065950037262a548f5a2a9d
Lee gun joon <kirkee2@naver.com>
2016-10-17 10:30:22 +0900

    ViewFlipper added

--- app/src/main/java/com/example/lkj/seoul/Fragment/MainFragment.java
+++ app/src/main/java/com/example/lkj/seoul/Fragment/MainFragment.java
@@ -7,11 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.example.lkj.seoul.R;
 
 public class MainFragment extends Fragment {
 
+    private ViewFlipper viewFlipper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,14 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        viewFlipper = (ViewFlipper) view.findViewById(R.id.viewFlipper);
+
+        viewFlipper.startFlipping();
+
+
+
+
         return view;
     }
 

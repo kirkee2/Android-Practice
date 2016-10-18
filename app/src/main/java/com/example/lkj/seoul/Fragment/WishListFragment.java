104bfb5321f2215b5fd4f2d7ffd164e6f834ec71
Lee gun joon <kirkee2@naver.com>
2016-10-19 03:24:00 +0900

    listview error fixed

--- app/src/main/java/com/example/lkj/seoul/Fragment/WishListFragment.java
+++ app/src/main/java/com/example/lkj/seoul/Fragment/WishListFragment.java
@@ -34,7 +34,7 @@ public class WishListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_community, container, false);
+        View view = inflater.inflate(R.layout.fragment_wishlist, container, false);
 
 
         mainLists = new ArrayList<MainList>();
